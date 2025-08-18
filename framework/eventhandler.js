a41ab0e5c4ea54469e6aa8d9827d81742804a278
ifares <hmsteven37@gmail.com>
2025-08-18 17:04:43 +0100

    -

--- framework/eventhandler.js
+++ framework/eventhandler.js
@@ -1,20 +1,32 @@
 export class EventRegistry {
-  constructor() {
-    this.listeners = new Map();
-  }
-
-  subscribe(eventType, callback) {
-    if (!this.listeners.has(eventType)) this.listeners.set(eventType, new Set());
-    this.listeners.get(eventType).add(callback);
-
-    // Return unsubscribe function
-    return () => this.listeners.get(eventType).delete(callback);
-  }
-
-  dispatch(eventType, payload) {
-    const cbs = this.listeners.get(eventType);
-    if (cbs) {
-      for (const cb of cbs) cb(payload);
+    constructor() {
+        this.listeners = new Map();
     }
-  }
+
+    subscribe(eventType, callback) {
+        if (!this.listeners.has(eventType)) this.listeners.set(eventType, new Set());
+        this.listeners.get(eventType).add(callback);
+
+        // Return unsubscribe function
+        return () => this.listeners.get(eventType).delete(callback);
+    }
+
+    dispatch(eventType, payload) {
+        const cbs = this.listeners.get(eventType);
+        if (cbs) {
+            for (const cb of cbs) cb(payload);
+        }
+    }
+    unsubscribe(eventName, callback) {
+        if (this.listeners.has(eventName)) {
+            this.listeners.get(eventName).delete(callback);
+        }
+    }
+
+    clear(eventName) {
+        // Remove all listeners for an event
+        if (this.listeners.has(eventName)) {
+            this.listeners.get(eventName).clear();
+        }
+    }
 }
